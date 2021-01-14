bf6db4f480d4c8ff0f867cdfdf6df8ba6395ae70
Antoine Cruveilher <acruveilher@free.fr>
2021-01-14 17:06:02 +0100

    Test antoine

--- MigDocs2.java
+++ MigDocs2.java
@@ -4,5 +4,6 @@ import java.io.File;
 public class MigDocs2 {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("Hello, c'est Antoine sur le repo");
     }
 }
