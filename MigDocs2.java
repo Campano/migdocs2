2403ff0add0f2bae05a7b1f2e399c6fb42aa6496
Antoine Cruveilher <acruveilher@free.fr>
2021-01-21 12:02:48 +0100

    nouvelle fonction traitement de dossier seulement

--- MigDocs2.java
+++ MigDocs2.java
@@ -12,6 +12,8 @@ public class MigDocs2 {
     // private final static String DOCS2_PATH = "/path/to/destination";
     private final static String DOCS2_PATH = "/Users/antoinecruveilher/Desktop";
     private final static Pattern MD_FILE_PATTERN = Pattern.compile(".*?([a-zA-Z0-9_-]+\\.md).*");
+    // Déclarer la liste "ordre des leçons"
+    public static ArrayList < String > lessonOrder = new ArrayList < > ();
 
     public static void main(String[] args) {
         System.out.println("Welcome to MigDocs2 tool");
@@ -33,7 +35,7 @@ public class MigDocs2 {
         // Déclarer la variable "répertoire cible"
         File target = new File(DOCS2_PATH);
         // Déclarer la variable "fichiers traîtés"
-        ArrayList<File> fichierTraités = new ArrayList<>();
+        ArrayList < File > fichierTraités = new ArrayList < > ();
 
         if (!origin.exists() || !target.exists())
             throw new MigDocs2Exception("MIG_ERR_BASE_DIR_NOT_FOUND");
@@ -43,11 +45,10 @@ public class MigDocs2 {
         if (!indexMd.exists())
             throw new MigDocs2Exception("MIG_ERR_INDEX_MD_NOT_FOUND", indexMd);
 
-        // Déclarer la liste "ordre des leçons"
-        ArrayList<String> lessonOrder = new ArrayList<>();
+
         try {
             // Pour chaque ligne du fichier "index.md"
-            for (String line : Files.readAllLines(Paths.get(indexMd.getPath()), Charset.forName("UTF-8"))) {
+            for (String line: Files.readAllLines(Paths.get(indexMd.getPath()), Charset.forName("UTF-8"))) {
                 // Si on détecte un nom de fichier md (via une regex "alphanumériques, tirets,
                 // underscores + '.md'")
                 Matcher m = MD_FILE_PATTERN.matcher(line);
@@ -61,26 +62,68 @@ public class MigDocs2 {
             throw new MigDocs2Exception("MIG_ERR_READING_INDEX");
         }
 
-        for (String lesson : lessonOrder)
+        for (String lesson: lessonOrder)
             System.out.println(lesson);
 
         //Créer la catégorie "CTG_50_docs" et son fichier json dans le répertoire cible
-        File theDir = new File(DOCS2_PATH+"/CTG_50_docs");
-        if (!theDir.exists()){
+        File theDir = new File(DOCS2_PATH + "/CTG_50_docs");
+        if (!theDir.exists()) {
             theDir.mkdirs();
 
-        //Fonction "Traiter contenu dossier" ("répertoire origine", "CTG_50_docs")
+            //Fonction "Traiter contenu dossier" ("répertoire origine", "CTG_50_docs")
 
 
-        
+
 
 
         }
     }
 
-    /*public static void dealFolderContent (File origin, File target){
-        int cmp = 
-    }*/
+    public static void dealFolderContent(File origin, File target) {
+        //utile pour le nom de la lesson
+        String tmp = "";
+        //Initialiser compteur ordre à 0
+        Integer order = 0;
+
+        //Déclarer la liste ordonnée "leçons de ce dossier"
+        ArrayList < String > LessonsOfThisFile = new ArrayList < > ();
+        //Pour chaque item de la liste "ordre des leçons"
+        for (String lesson: lessonOrder) {
+            //Si la leçon existe dans ce dossier, ajouter à la liste "leçons de ce dossier"
+            File currentLesson = new File(origin, lesson);
+            if (currentLesson.exists()) {
+                LessonsOfThisFile.add(lesson);
+            }
+        }
+        //Pour chaque fichier de ce dossier
+        File[] files = origin.listFiles();
+
+        for (int i = 0; i <= files.length; ++i) {
+            //Si c'est un dossier
+            if (!files[i].getName().contains(".")) {
+                //+10 au compteur ordre 
+                order += 10;
+                Pattern p = Pattern.compile("[a-z]");
+                Matcher m = p.matcher(files[i].getName());
+                //Lire le nom du dossier (du type "01-core")
+                //Détecter le nom de la catégorie ("core" via une regex: deux chiffres + un tiret + alphanum)
+                //Si ne correspond pas, renvoyer erreur
+                if (m.matches()) {
+                    tmp = m.group(1);
+                }
+                //Créer le dossier catégorie (type "CTG_10_core") avec l'ordre et son fichier json dans "CTG_50_docs"
+                File newCategory = new File(DOCS2_PATH + "/CTG_50_docs/CTG_" + order + "_" + tmp);
+                if (!newCategory.exists()) {
+                    newCategory.mkdirs();
+                }
+                File nJSON = new File(DOCS2_PATH + "/CTG_50_docs/CTG_" + order + "_" + tmp + "/category.json");
+                //écriture dans le json pas encore faite
+                dealFolderContent(files[i], newCategory);
+            }
+
+        }
+
+    }
 
     public static class MigDocs2Exception extends Exception {
         private String additional;
